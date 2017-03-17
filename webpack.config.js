001e1d3283e36c5dca7993b0e6cbb49309bf1b20
yuan <yuan@yuandeMacBook-Pro.local>
2017-03-17 18:33:24 +0800

    antd

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: [
+      'webpack/hot/only-dev-server',
+      "./src/step/app.js"
+    ],
+    output: {
+        path: './build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.js?$/, exclude: /node_modules/, loaders: ['react-hot','babel-loader?presets[]=react,presets[]=es2015'] },
+            { test: /\.css$/, loader: 'style!css'}
+        ]
+    },
+    resolve:{
+        extensions:['','.js','.json']
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin()
+    ]
+};
