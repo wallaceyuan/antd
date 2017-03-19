9dace9986981199cc0cb98d672b5cb03b61beb57
wallaceyuan <1272864289@qq.com>
2017-03-19 20:27:58 +0800

    test

--- src/table/ExampleTable.js
+++ src/table/ExampleTable.js
@@ -3,7 +3,11 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { DatePicker, message, Button,Dropdown,Menu ,Icon } from 'antd';
+import { DatePicker, message, Button,Dropdown,Menu ,Icon,Layout,  } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+const { Header, Footer, Sider, Content } = Layout;
+
 import 'antd/dist/antd.css';
 
 
@@ -13,7 +17,8 @@ class ExampleTable extends React.Component {
         super(props);
         this.state = {
             date: '',
-            size: 'large'
+            size: 'large',
+            current: 'mail',
         };
     }
     handleChange(date) {
@@ -34,7 +39,37 @@ class ExampleTable extends React.Component {
 
         const size = this.state.size;
         return (
-            <div style={{ width: 400, margin: '100px auto' }}>
+            <div style={{ width: 800, margin: '100px auto' }}>
+                <Menu
+                    onClick={this.handleClick}
+                    selectedKeys={[this.state.current]}
+                    mode="horizontal"
+                >
+                    <Menu.Item key="mail">
+                        <Icon type="mail" />Navigation One
+                    </Menu.Item>
+                    <Menu.Item key="app" disabled>
+                        <Icon type="appstore" />Navigation Two
+                    </Menu.Item>
+                    <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
+                        <MenuItemGroup title="Item 1">
+                            <Menu.Item key="setting:1">Option 1</Menu.Item>
+                            <Menu.Item key="setting:2">Option 2</Menu.Item>
+                        </MenuItemGroup>
+                        <MenuItemGroup title="Item 2">
+                            <Menu.Item key="setting:3">Option 3</Menu.Item>
+                            <Menu.Item key="setting:4">Option 4</Menu.Item>
+                        </MenuItemGroup>
+                    </SubMenu>
+                    <Menu.Item key="alipay">
+                        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+                    </Menu.Item>
+                </Menu>
+                <Layout>
+                    <Header>Header</Header>
+                    <Content>Content</Content>
+                    <Footer>Footer</Footer>
+                </Layout>
                 <Button type="primary" shape="circle" icon="search" />
                 <Button type="primary">Primary</Button>
                 <DatePicker onChange={value => this.handleChange(value)} />
@@ -44,6 +79,7 @@ class ExampleTable extends React.Component {
                         more <Icon type="down" />
                     </Button>
                 </Dropdown>
+                <Icon type="step-backward" />
                 <div style={{ marginTop: 20 }}>Date: {this.state.date.toString()}</div>
             </div>
         );
